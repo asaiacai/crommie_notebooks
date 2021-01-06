5186531d96faae15dd06f834c4d60b570e65c00b
Andrew Aikawa <asai@ocf.berkeley.edu>
2021-01-05 16:46:15 -0800

    Jan 5th snapshot

--- sxmreader.py
+++ sxmreader.py
@@ -1,6 +1,7 @@
 import pims
 import pySPM as spm
 import numpy as np
+from skimage.transform import resize
 
 #@pims.pipeline
 #def gray(image):
@@ -10,24 +11,36 @@ import numpy as np
 class SXMReader(pims.FramesSequence):
     def __init__(self, filename_pattern):
         #self.filenames = glob.glob(filename_pattern)
+        self.aaaa="aaaa"
         self.filenames = filename_pattern
         self.scans = [spm.SXM(filename) for filename in self.filenames]
-        self.z_data = np.array([s.get_channel("Z").correct_lines().pixels for s in self.scans])
-        print(self.z_data)
-        min_z = np.amin(self.z_data)
-        max_z = np.amax(self.z_data)
-        self.z_data -= min_z
-        self.z_data /= max_z-min_z
+        self.smallest = 9999999
+        self.z_data = []
+        for i, s in enumerate(self.scans):
+            pxs = s.get_channel("Z").correct_lines().pixels
+            f = open(self.filenames[i], "r", encoding='latin-1')
+            lines = f.readlines()
+            scan_up = [lines[i+1 % len(lines)] for i, x in enumerate(lines) if x == ':SCAN_DIR:\n'][0].strip() == 'up'
+            if not scan_up:
+                pxs = pxs[::-1]
+            self.smallest = min(self.smallest, pxs.shape[0])
+            self.z_data.append(pxs)
         self._len = len(self.z_data)
-        self._dtype = self.z_data.dtype
-        self._frame_shape = self.z_data[0].shape
+        self._dtype = np.float32
+        self._frame_shape = (self.smallest, self.smallest)
         self.scan_size = self.scans[0].size
-        self.meters_per_pixel = self.scan_size['real']['x']/self.scan_size['pixels']['x']
+        self.meters_per_pixel = self.scan_size['real']['x']/ self.smallest
        
     def get_frame(self, i):
         # Access the data you need and get it into a numpy array.
         # Then return a Frame like so:
-        return pims.Frame(self.z_data[i], frame_no=i)
+        image = self.z_data[i]
+        min_z = np.amin(image)
+        max_z = np.amax(image)
+        image -= min_z
+        image /= max_z-min_z
+        image = resize(image, self.frame_shape)
+        return pims.Frame(image, frame_no=i)
 
     def __len__(self):
         return self._len
